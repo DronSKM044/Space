341755f878e31f67fc2c14c3d2bb1f1c8adde110
Andrii Lishchyna <andrej044@i.ua>
2022-05-09 19:11:15 +0200

    animated rotation text

--- js/rotateText.js
+++ js/rotateText.js
@@ -1,8 +1,23 @@
 `use strict`;
 
-export function rotateMain() {
-  console.log("export rotateMain");
+export function rotateMain(value) {
+  const inner = document.querySelectorAll(".landing-signature__inner");
+  let deg = 0;
+
+  for (let item of inner) {
+    item.style.transform = `rotate(${deg}deg)`;
+    deg += Math.sin(value / 200) + Math.cos(value * 0.01);
+  }
+  requestAnimationFrame(rotateMain);
+  return value;
 }
-export function rotateOuter() {
-  console.log("export rotateOuter");
+
+export function rotateOuter(value) {
+  const content = document.querySelectorAll(".signature");
+  let deg = 0;
+
+  for (let item of content) {
+    item.style.transform = `rotate(${deg}deg)`;
+    deg += value;
+  }
 }
---
+++ js/script.js
@@ -0,0 +1,7 @@
+`use strict`;
+import { rotateMain, rotateOuter } from "./rotateText.js";
+
+window.onload = function () {
+  rotateMain(100);
+  rotateOuter(10);
+};
