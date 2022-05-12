935fe542a1d4ce51e1870b9edc47e61e313b1c21
Andrii Lishchyna <andrej044@i.ua>
2022-05-12 13:41:25 +0200

    add rotate3D object

---
+++ js/rotateItem3D.js
@@ -0,0 +1,18 @@
+`use strict`;
+
+export function rotate3D(el) {
+  let halfHeight = null;
+  let startY;
+  if (el !== undefined) {
+    el.addEventListener("mousemove", (e) => {
+      halfHeight = el.offsetHeight / 2;
+      console.log(startY);
+      el.style.transform = `rotateX(${
+        (e.offsetY - halfHeight) / 30
+      }deg) rotateY(${(e.offsetX - halfHeight) / 30}deg) `;
+    });
+    el.addEventListener("mouseout", () => {
+      el.style.transform = `rotate(0)`;
+    });
+  } else console.log("need element");
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 `use strict`;
 import { rotateMain, rotateOuter } from "./rotateText.js";
+import { rotate3D } from "./rotateItem3D.js";
 
 window.onload = function () {
   rotateMain(100);
   rotateOuter(10);
+
+  const cards = document.querySelectorAll(".picture-link");
+  for (let item of cards) {
+    rotate3D(item);
+  }
 };
